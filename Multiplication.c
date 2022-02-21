ef9a03c6c9d894cbfbe161b5d485d875494ce40a
VENKAT.M <100109719+ACTIONBOLT1127@users.noreply.github.com>
2022-02-21 10:59:54 +0530

    #include<stdio.h> int main() {     int a,b, multiplication;     //printf("enter the a,b values");     scanf("%d%d",&a,&b);     multiplication=a*b;     printf("%d", multiplication);     return 0;
    }

---
+++ Multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b, multiplication;
+    //printf("enter the a,b values");
+    scanf("%d%d",&a,&b);
+    multiplication=a*b;
+    printf("%d", multiplication);
+    return 0;
+
+}
