7df381444e9def3ad2dc383f223e3262b9f1e647
VENKAT.M <100109719+ACTIONBOLT1127@users.noreply.github.com>
2022-02-21 11:00:54 +0530

    #include<stdio.h> int main() {     int a,b,modulus;     //printf("enter the a,b values");     scanf("%d%d%d",&a,&b);     modulus=a%b;     printf("%d",modulus);     return 0; }

---
+++ Modulus.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,modulus;
+    //printf("enter the a,b values");
+    scanf("%d%d%d",&a,&b);
+    modulus=a%b;
+    printf("%d",modulus);
+    return 0;
+}
