0ae6a564a112439eb0d8f2aa85f46d2b2492bac4
VENKAT.M <100109719+ACTIONBOLT1127@users.noreply.github.com>
2022-02-21 11:00:23 +0530

    #include<stdio.h> int main() {     int a,b,division;     //printf("enter the a,b values");     scanf("%d%d",&a,&b);     division=a/b;     printf("%d", division);     return 0; }

---
+++ Division.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,division;
+    //printf("enter the a,b values");
+    scanf("%d%d",&a,&b);
+    division=a/b;
+    printf("%d", division);
+    return 0;
+}
