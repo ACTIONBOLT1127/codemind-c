bb99f9e7c29599fea04c0f27764c59a380e97e0b
VENKAT.M <100109719+ACTIONBOLT1127@users.noreply.github.com>
2022-02-28 09:47:23 +0530

    #include<stdio.h> int main() {     int n,i,a[50];     scanf("%d",&n);     a[0]=0;     a[1]=1;     for(i=1;i<n+1;i++)     {         a[i+1]=a[i]+a[i-1];     }     for(i=0;i<n;i++)     {         printf("%d ",a[i]);     } }

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[50];
+    scanf("%d",&n);
+    a[0]=0;
+    a[1]=1;
+    for(i=1;i<n+1;i++)
+    {
+        a[i+1]=a[i]+a[i-1];
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
